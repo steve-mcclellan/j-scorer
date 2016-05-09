c08defc6a283b5698adee0192f6b9d4e19cac7b5
Steve McClellan <steve470@gmail.com>
2016-05-09 19:36:23 -0400

    Test for expired reset token; efficientize a database hit

--- app/models/user.rb
+++ app/models/user.rb
@@ -44,8 +44,8 @@ class User < ActiveRecord::Base
   # Sets the password reset attributes.
   def create_reset_digest
     self.reset_token = User.new_token
-    update_attribute(:reset_digest, User.digest(reset_token))
-    update_attribute(:reset_sent_at, Time.zone.now)
+    update_columns(reset_digest:  User.digest(reset_token),
+                   reset_sent_at: Time.zone.now)
   end
 
   # Sends password reset email.
--- test/integration/password_resets_test.rb
+++ test/integration/password_resets_test.rb
@@ -49,4 +49,18 @@ class PasswordResetsTest < ActionDispatch::IntegrationTest
     assert_not flash.empty?
     assert_redirected_to root_url
   end
+
+  test 'expired token' do
+    get forgot_path
+    post forgot_path, password_reset: { email: @user.email }
+
+    @user = assigns(:user)
+    @user.update_attribute(:reset_sent_at, 90.minutes.ago)
+    patch reset_path(@user.reset_token),
+          email: @user.email,
+          user: { password: 'foobar', password_confirmation: 'foobar' }
+    assert_response :redirect
+    follow_redirect!
+    assert_match(/expired/i, response.body)
+  end
 end
