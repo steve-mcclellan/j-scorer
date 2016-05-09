8f2f9c8bf04625906226107d5a6af3d8afb28d9f
Steve McClellan <steve470@gmail.com>
2016-05-09 16:52:30 -0400

    Improve friendly forwarding and site layout tests

--- test/integration/friendly_forwarding_test.rb
+++ test/integration/friendly_forwarding_test.rb
@@ -8,7 +8,9 @@ class FriendlyForwardingTest < ActionDispatch::IntegrationTest
   test 'get stats page via friendly forwarding' do
     get stats_path
     assert_redirected_to login_path
+    assert_not_nil session[:forwarding_url]
     log_in_here(@user)
     assert_redirected_to stats_path
+    assert_nil session[:forwarding_url]
   end
 end
--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -1,12 +1,30 @@
 require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:dave)
+  end
+
   test 'layout links' do
     get root_path
     assert_template 'pages/home'
     assert_select 'a[href=?]', root_path, count: 2
     assert_select 'a[href=?]', help_path
     assert_select 'a[href=?]', about_path
+    assert_select 'a[href=?]', login_path
+    assert_select 'a[href=?]', signup_path
+    assert_select 'a[href=?]', stats_path, count: 0
+    assert_select 'a[href=?]', logout_path, count: 0
+    log_in_here @user
+    get root_path
+    assert_template 'pages/home'
+    assert_select 'a[href=?]', root_path, count: 2
+    assert_select 'a[href=?]', help_path
+    assert_select 'a[href=?]', about_path
+    assert_select 'a[href=?]', login_path, count: 0
+    assert_select 'a[href=?]', signup_path, count: 0
+    assert_select 'a[href=?]', stats_path, count: 2
+    assert_select 'a[href=?]', logout_path
   end
 
   test 'signup page should show up' do
