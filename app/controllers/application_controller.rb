40607ecdcb6f286034d46ef684f53723772f5664
Steve McClellan <steve470@gmail.com>
2016-05-08 21:34:40 -0400

    Redirect all (production) requests to https://j-scorer.com

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,13 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   include SessionsHelper
+
+  # Suggested at stackoverflow.com/questions/6701549/heroku-ssl-on-root-domain
+  before_action :check_domain
+
+  def check_domain
+    if Rails.env.production? && request.host.downcase != 'j-scorer.com'
+      redirect_to 'https://j-scorer.com' + request.fullpath, status: 301
+    end
+  end
 end
