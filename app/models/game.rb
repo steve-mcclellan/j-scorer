a1404f7ae4d2b79c50411e6844f57c3c90afeee8
Steve McClellan <steve470@gmail.com>
2016-06-06 20:29:01 -0400

    Move date_played of most_recent game into future

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,7 +1,5 @@
 class Game < ActiveRecord::Base
   belongs_to :user, touch: true
-  # has_many :round_one_categories, dependent: :destroy
-  # has_many :round_two_categories, dependent: :destroy
   has_many :sixths, dependent: :destroy
   has_one :final, dependent: :destroy
 
