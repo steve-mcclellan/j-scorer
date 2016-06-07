a44ca544550addc8cd23761a77af36e10e48d432
Steve McClellan <steve470@gmail.com>
2016-06-07 19:13:28 -0400

    Restrict domain check to production site only

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,10 @@ class ApplicationController < ActionController::Base
   before_action :check_domain
 
   def check_domain
-    if Rails.env.production? && request.host.casecmp(ENV['PROPER_DOMAIN']) != 0
+    if Rails.env.production? &&
+       ENV['PROPER_DOMAIN'] &&
+       request.host.casecmp(ENV['PROPER_DOMAIN']) != 0
+
       proper_url = "https://#{ENV['PROPER_DOMAIN']}#{request.fullpath}"
       redirect_to proper_url, status: 301
     end
