f50fb54fa696c6de91e44e26e9a4e0f992cac0b6
Steve McClellan <steve470@gmail.com>
2016-11-27 16:42:54 -0500

    Update JavaScript to reflect unified stats page

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -12,33 +12,7 @@ $( ".users-show, .users-sample" ).ready( function() {
     return types;
   }
 
-  $( "#stats-area" ).tabs({
-    // Ajax request stuff, mostly pulled from jQuery UI's docs.
-    beforeLoad: function( event, ui ) {
-
-      // There's no need to do a fresh Ajax request each time the tab
-      // is clicked. If the tab has been loaded already (or is currently
-      // loading), skip the request and display the pre-existing data
-      // (or continue with the current request).
-      if ( ui.tab.data( "loaded" ) || ui.tab.data( "loading" ) ) {
-        event.preventDefault();
-        return;
-      }
-
-      ui.tab.data( "loading", true );
-      ui.panel.html( "Retrieving data - one moment please..." );
-
-      ui.jqXHR.success( function() {
-        ui.tab.data( "loading", false );
-        ui.tab.data( "loaded", true );
-      });
-
-      ui.jqXHR.fail( function() {
-        ui.tab.data( "loading", false );
-        ui.panel.html( "Couldn't load this tab. Please try again later." );
-      });
-    }
-  });
+  $( "#stats-area" ).tabs();
 
   var $gameTable = $( "#gameTable" );
   var $typeTable = $( "#typeTable" );
@@ -66,7 +40,22 @@ $( ".users-show, .users-sample" ).ready( function() {
     scrollableArea: $( '#stats-area' )
   });
 
-  // Similar for "Play types" table. Initialize default sort to
+  // Similar for the "Topics" table.
+  // Default sort: [ topic name, ascending ].
+  // Make everything else sort descending on first click.
+  $( "#topicTable" ).tablesorter({
+    sortList: [[0,0]],
+    sortInitialOrder: "desc",
+    headers: {
+      0: { sortInitialOrder: "asc" }
+    }
+  });
+
+  $( "#topicTable" ).stickyTableHeaders({
+    scrollableArea: $( '#stats-area' )
+  });
+
+  // And similar for "Play types" table. Initialize default sort to
   // [ Games played, descending ]. Prevent meaningless attempts
   // to sort by checkbox column (currently in position 0).
   // To prevent a JS error, skip this if the table is empty.
@@ -125,8 +114,4 @@ $( ".users-show, .users-sample" ).ready( function() {
       window.location.replace( url );
     }
   });
-
-  // Set the topics tab (currently in position 2) to load in the background
-  // as soon as everything else is done.
-  $( "#stats-area" ).tabs( "load", 2 );
 });
