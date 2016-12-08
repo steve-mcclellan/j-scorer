334db9a5a8dc3e56b5cbf9611e1ac0b469bbf355
Steve McClellan <steve470@gmail.com>
2016-12-08 15:02:40 -0500

    Prevent password reset tokens from being reused

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -2,6 +2,7 @@ class PasswordResetsController < ApplicationController
   before_action :find_user,        only: [:edit, :update]
   before_action :valid_user,       only: [:edit, :update]
   before_action :check_expiration, only: [:edit, :update]
+  before_action :password_present, only: [:update]
 
   def new
   end
@@ -21,11 +22,9 @@ class PasswordResetsController < ApplicationController
   end
 
   def update
-    if params[:user][:password].empty?
-      @user.errors.add(:password, "can't be empty")
-      render 'edit'
-    elsif @user.update_attributes(user_params)
+    if @user.update(user_params)
       log_in @user
+      @user.update_attribute(:reset_digest, nil)
       flash[:success] = 'Password has been reset.'
       redirect_to root_url
     else
@@ -45,18 +44,23 @@ class PasswordResetsController < ApplicationController
     @user = User.find_by(email: params[:email])
   end
 
-  # Confirms a valid user.
   def valid_user
     unless @user && @user.authenticated?(:reset, params[:id])
       redirect_to root_url
     end
   end
 
-  # Checks expiration of reset token.
   def check_expiration
     if @user.password_reset_expired?
       flash[:danger] = 'Password reset has expired.'
       redirect_to forgot_url
+    end
+  end
+
+  def password_present
+    if params[:user][:password].empty?
+      @user.errors.add(:password, "can't be empty")
+      render 'edit'
     end
   end
 end
