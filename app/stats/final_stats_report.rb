b93e9306a821117ad6d3cdb6f775e6237ddb3938
Steve McClellan <steve470@gmail.com>
2016-12-26 13:29:15 -0500

    Update FinalStatsReport to reflect format changes
    FinalStatsReport uses
    ActiveRecord::Base.connection.select_all(query_string).to_hash
    to get its raw materials from the database. This provides an array of
    hashes, one hash for each result.
    
    Formerly, the values in these hashes would all be strings (or nil). An
    integer value of 3 would come back as '3'; Booleans would be 't' or 'f'.
    
    In Rails 5, the values come back in their original formats.
    
    This commit adjusts the data processing to reflect the format change
    (including removing the now-obsolete 'booleanize' method). It also
    refactors a bit, eliminating the need for a Rubocop exception.

--- app/stats/final_stats_report.rb
+++ app/stats/final_stats_report.rb
@@ -14,6 +14,15 @@ class FinalStatsReport
 
   private
 
+  def prepare_hash(finals)
+    finals.map! do |final|
+      { user_right: final['result'].to_i == 3,
+        contestants_right: [final['first_right'],
+                            final['second_right'],
+                            final['third_right']] }
+    end
+  end
+
   def crunch_numbers(finals)
     @stats = { user: [0, 0], contestants: [nil, [0, 0], [0, 0], [0, 0]],
                by_get_rate: [[[0, 0], [0, 0], [0, 0], [0, 0]],
@@ -35,13 +44,9 @@ class FinalStatsReport
     end
   end
 
-  def add_by_contestant_result(final)
-    (1..3).each do |i|
-      case final[:contestants_right][i - 1]
-      when true then add_final_to(@stats[:when_right][i], final)
-      when false then add_final_to(@stats[:when_wrong][i], final)
-      end
-    end
+  def add_raw_results(final)
+    add_final_to(@stats[:user], final)
+    (1..3).each { |i| add_final_to(@stats[:contestants][i], final, i) }
   end
 
   def add_by_get_rate(final)
@@ -56,41 +61,12 @@ class FinalStatsReport
     add_final_to(@stats[:by_get_rate][correct_responders][participants], final)
   end
 
-  def add_raw_results(final)
-    add_final_to(@stats[:user], final)
-    (1..3).each { |i| add_final_to(@stats[:contestants][i], final, i) }
-  end
-
-  def prepare_hash(finals)
-    finals.map! do |final|
-      { user_right: final['result'] == '3',
-        contestants_right: [booleanize(final['first_right']),
-                            booleanize(final['second_right']),
-                            booleanize(final['third_right'])] }
-    end
-  end
-
-  # rubocop:disable AbcSize
-  def post_processing
-    sc = @stats[:contestants]
-    sc[0] = sc[1..3].transpose.map { |arr| arr.reduce(:+) }
-
-    arrs = [@stats[:user]] + @stats[:contestants] + @stats[:by_get_rate][0] +
-           @stats[:by_get_rate][1][1..3] + @stats[:by_get_rate][2][2..3] +
-           [@stats[:by_get_rate][3][3]] + @stats[:when_right][1..3] +
-           @stats[:when_wrong][1..3]
-
-    arrs.each do |arr|
-      arr[1] += arr[0]
-      arr << quotient(arr[0], arr[1])
-    end
-  end
-  # rubocop:enable AbcSize
-
-  def booleanize(data)
-    case data
-    when 't' then true
-    when 'f' then false
+  def add_by_contestant_result(final)
+    (1..3).each do |i|
+      case final[:contestants_right][i - 1]
+      when true then add_final_to(@stats[:when_right][i], final)
+      when false then add_final_to(@stats[:when_wrong][i], final)
+      end
     end
   end
 
@@ -100,6 +76,28 @@ class FinalStatsReport
     case result
     when true then arr[0] += 1
     when false then arr[1] += 1
+    end
+  end
+
+  def post_processing
+    # Add the contestants' overall stats.
+    sc = @stats[:contestants]
+    sc[0] = sc[1..3].transpose.map { |arr| arr.reduce(:+) }
+
+    add_averages
+  end
+
+  def add_averages
+    # Assemble the arrays that contain get rates.
+    arrs = ([@stats[:user]] + @stats[:contestants] +
+            @stats[:by_get_rate].flatten(1) +
+            @stats[:when_right] + @stats[:when_wrong]).compact
+
+    # Change the format of each array from [right, wrong] to [right, total]
+    # and tack on the get rate as a float between 0 and 1.
+    arrs.each do |arr|
+      arr[1] += arr[0]
+      arr << quotient(arr[0], arr[1])
     end
   end
 end
