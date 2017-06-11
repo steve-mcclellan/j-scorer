d2bf77a95a414fcaf1e99a5b343bdc47c27adddf
Steve McClellan <steve470@gmail.com>
2017-06-11 17:04:10 -0400

    Move MultiGameSummary logic from Ruby to SQL

--- app/stats/multi_game_queries.rb
+++ app/stats/multi_game_queries.rb
@@ -1,16 +1,83 @@
 # These queries use the valid_types? method from the SharedStatsMethods module.
 module MultiGameQueries
+  # rubocop:disable MethodLength
   def sixths_query(user, play_types)
     raise ArgumentError unless user.is_a?(User) && valid_types?(play_types)
     play_types_list = play_types.map { |x| "'#{x}'" }.join(', ')
 
     "
-    SELECT s.type, s.result1, s.result2, s.result3, s.result4, s.result5
-    FROM sixths s
-    INNER JOIN games g ON g.id = s.game_id
-    WHERE g.user_id = #{user.id} AND g.play_type IN (#{play_types_list})
+    SELECT
+      (
+        CASE
+          WHEN c.type = 'RoundOneCategory' THEN 1
+          WHEN c.type = 'RoundTwoCategory' THEN 2
+        END
+      ) AS round_number,
+      (
+        COUNT(*) FILTER (WHERE c.result1 = 3) +
+        COUNT(*) FILTER (WHERE c.result2 = 3) +
+        COUNT(*) FILTER (WHERE c.result3 = 3) +
+        COUNT(*) FILTER (WHERE c.result4 = 3) +
+        COUNT(*) FILTER (WHERE c.result5 = 3)
+      ) AS right,
+      (
+        COUNT(*) FILTER (WHERE c.result1 = 1) +
+        COUNT(*) FILTER (WHERE c.result2 = 1) +
+        COUNT(*) FILTER (WHERE c.result3 = 1) +
+        COUNT(*) FILTER (WHERE c.result4 = 1) +
+        COUNT(*) FILTER (WHERE c.result5 = 1)
+      ) AS wrong,
+      (
+        COUNT(*) FILTER (WHERE c.result1 = 2) +
+        COUNT(*) FILTER (WHERE c.result2 = 2) +
+        COUNT(*) FILTER (WHERE c.result3 = 2) +
+        COUNT(*) FILTER (WHERE c.result4 = 2) +
+        COUNT(*) FILTER (WHERE c.result5 = 2)
+      ) AS pass,
+      (
+        COUNT(*) FILTER (WHERE c.result1 = 7) +
+        COUNT(*) FILTER (WHERE c.result2 = 7) +
+        COUNT(*) FILTER (WHERE c.result3 = 7) +
+        COUNT(*) FILTER (WHERE c.result4 = 7) +
+        COUNT(*) FILTER (WHERE c.result5 = 7)
+      ) AS dd_right,
+      (
+        COUNT(*) FILTER (WHERE c.result1 IN (5, 6)) +
+        COUNT(*) FILTER (WHERE c.result2 IN (5, 6)) +
+        COUNT(*) FILTER (WHERE c.result3 IN (5, 6)) +
+        COUNT(*) FILTER (WHERE c.result4 IN (5, 6)) +
+        COUNT(*) FILTER (WHERE c.result5 IN (5, 6))
+      ) AS dd_wrong,
+      (
+        (1 * COUNT(*) FILTER (WHERE c.result1 IN (3, 7))) +
+        (2 * COUNT(*) FILTER (WHERE c.result2 IN (3, 7))) +
+        (3 * COUNT(*) FILTER (WHERE c.result3 IN (3, 7))) +
+        (4 * COUNT(*) FILTER (WHERE c.result4 IN (3, 7))) +
+        (5 * COUNT(*) FILTER (WHERE c.result5 IN (3, 7))) +
+        (-1 * COUNT(*) FILTER (WHERE c.result1 = 1)) +
+        (-2 * COUNT(*) FILTER (WHERE c.result2 = 1)) +
+        (-3 * COUNT(*) FILTER (WHERE c.result3 = 1)) +
+        (-4 * COUNT(*) FILTER (WHERE c.result4 = 1)) +
+        (-5 * COUNT(*) FILTER (WHERE c.result5 = 1))
+      ) AS raw_score,
+      (
+        (1 * COUNT(*) FILTER (WHERE c.result1 IN (1, 2, 3, 5, 6, 7))) +
+        (2 * COUNT(*) FILTER (WHERE c.result2 IN (1, 2, 3, 5, 6, 7))) +
+        (3 * COUNT(*) FILTER (WHERE c.result3 IN (1, 2, 3, 5, 6, 7))) +
+        (4 * COUNT(*) FILTER (WHERE c.result4 IN (1, 2, 3, 5, 6, 7))) +
+        (5 * COUNT(*) FILTER (WHERE c.result5 IN (1, 2, 3, 5, 6, 7)))
+      ) AS raw_possible_score
+    FROM (
+      SELECT s.type, s.result1, s.result2, s.result3, s.result4, s.result5
+      FROM
+        sixths s
+        INNER JOIN games g ON g.id = s.game_id
+      WHERE g.user_id = #{user.id} AND g.play_type IN (#{play_types_list})
+    ) c
+    GROUP BY type
     "
   end
+  # rubocop:enable MethodLength
 
   def count_query(user, play_types)
     raise ArgumentError unless user.is_a?(User) && valid_types?(play_types)
--- app/stats/multi_game_summary.rb
+++ app/stats/multi_game_summary.rb
@@ -4,21 +4,24 @@ class MultiGameSummary
   attr_reader :stats
 
   def initialize(user, play_types)
-    sixths = ActiveRecord::Base.connection
-                               .select_all(sixths_query(user, play_types))
-                               .to_hash
+    round_info = ActiveRecord::Base.connection
+                                   .select_all(sixths_query(user, play_types))
+                                   .to_hash
 
     count = ActiveRecord::Base.connection
                               .select_all(count_query(user, play_types))
                               .to_hash[0]
 
-    crunch_numbers(sixths)
+    initialize_stats
+    reformat_data(round_info)
     add_calculated_stats(count)
   end
 
   private
 
-  def crunch_numbers(data)
+  def initialize_stats
+    # Most of this will be overwritten, but initialize it anyway to avoid
+    # errors if the round_info SQL comes back empty (e.g., for a new user).
     @stats = { round_one: { right: 0, wrong: 0, pass: 0,
                             dd_right: 0, dd_wrong: 0,
                             score: 0, possible_score: 0 },
@@ -28,20 +31,41 @@ class MultiGameSummary
                finals: { right: 0, wrong: 0 },
                all: {} }
 
-    data.each { |sixth_hash| add_sixth_to_stats(sixth_hash) }
+    # data.each { |sixth_hash| add_sixth_to_stats(sixth_hash) }
   end
 
-  def add_sixth_to_stats(sixth_hash)
-    round = sixth_hash['type'] == 'RoundOneCategory' ? 0 : 1
-    top_row_value = CURRENT_TOP_ROW_VALUES[round]
-    round_symbol = [:round_one, :round_two][round]
-
-    1.upto(5) do |row|
-      add_clue_to_stats(@stats[round_symbol],
-                        sixth_hash['result' + row.to_s],
-                        top_row_value * row)
+  def reformat_data(round_info)
+    round_info.each do |round_hash|
+      if round_hash['round_number'] == 1
+        @stats[:round_one] = convert_stats(round_hash)
+      elsif round_hash['round_number'] == 2
+        @stats[:round_two] = convert_stats(round_hash)
+      end
     end
   end
+
+  def convert_stats(round_hash)
+    top_row_value = CURRENT_TOP_ROW_VALUES[round_hash['round_number'] - 1]
+    { right: round_hash['right'],
+      wrong: round_hash['wrong'],
+      pass: round_hash['pass'],
+      dd_right: round_hash['dd_right'],
+      dd_wrong: round_hash['dd_wrong'],
+      score: round_hash['raw_score'] * top_row_value,
+      possible_score: round_hash['raw_possible_score'] * top_row_value }
+  end
+
+  # def add_sixth_to_stats(sixth_hash)
+  #   round = sixth_hash['type'] == 'RoundOneCategory' ? 0 : 1
+  #   top_row_value = CURRENT_TOP_ROW_VALUES[round]
+  #   round_symbol = [:round_one, :round_two][round]
+
+  #   1.upto(5) do |row|
+  #     add_clue_to_stats(@stats[round_symbol],
+  #                       sixth_hash['result' + row.to_s],
+  #                       top_row_value * row)
+  #   end
+  # end
 
   def add_calculated_stats(count)
     @stats[:all][:game_count] = count['games'].to_i
