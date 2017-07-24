d94c91847e6f68d7a0e52202f18c037fbc311928
Steve McClellan <steve470@gmail.com>
2017-07-24 17:42:33 -0400

    Move FinalStatsReport logic from Ruby to SQL

--- app/stats/final_queries.rb
+++ app/stats/final_queries.rb
@@ -1,15 +1,176 @@
-# This module relies on valid_types? from the SharedStatsMethods module.
+# This module relies on validate_query_inputs from SharedStatsMethods.
+# rubocop:disable ModuleLength
 module FinalQueries
+  # rubocop:disable MethodLength
   def final_query(user, play_types)
-    raise ArgumentError unless user.is_a?(User) && valid_types?(play_types)
+    validate_query_inputs(user, play_types)
     play_types_list = play_types.map { |x| "'#{x}'" }.join(', ')
 
     "
-    SELECT f.result, f.first_right, f.second_right, f.third_right
-    FROM finals f
-    INNER JOIN games g ON g.id = f.game_id
-    WHERE g.user_id = #{user.id} AND g.play_type IN (#{play_types_list})
-      AND f.result IN (1, 3)
+    SELECT
+      COUNT(*) FILTER (WHERE result = 3) AS user_num,
+      COUNT(*) AS user_den,
+      SUM(contestants_right) AS contestants_num,
+      SUM(contestants_right) + SUM(contestants_wrong) AS contestants_den,
+      COUNT(*) FILTER (WHERE first_right) AS first_num,
+      COUNT(*) FILTER (WHERE first_right IS NOT NULL) AS first_den,
+      COUNT(*) FILTER (WHERE second_right) AS second_num,
+      COUNT(*) FILTER (WHERE second_right IS NOT NULL) AS second_den,
+      COUNT(*) FILTER (WHERE third_right) AS third_num,
+      COUNT(*) FILTER (WHERE third_right IS NOT NULL) AS third_den,
+      COUNT(*) FILTER (
+        WHERE result = 3 AND contestants_total = 0
+      ) AS zero_for_zero_num,
+      COUNT(*) FILTER (WHERE contestants_total = 0) AS zero_for_zero_den,
+      COUNT(*) FILTER (
+        WHERE result = 3 AND contestants_right = 0 AND contestants_total = 1
+      ) AS zero_for_one_num,
+      COUNT(*) FILTER (
+        WHERE contestants_right = 0 AND contestants_total = 1
+      ) AS zero_for_one_den,
+      COUNT(*) FILTER (
+        WHERE result = 3 AND contestants_right = 0 AND contestants_total = 2
+      ) AS zero_for_two_num,
+      COUNT(*) FILTER (
+        WHERE contestants_right = 0 AND contestants_total = 2
+      ) AS zero_for_two_den,
+      COUNT(*) FILTER (
+        WHERE result = 3 AND contestants_right = 0 AND contestants_total = 3
+      ) AS zero_for_three_num,
+      COUNT(*) FILTER (
+        WHERE contestants_right = 0 AND contestants_total = 3
+      ) AS zero_for_three_den,
+      COUNT(*) FILTER (
+        WHERE result = 3 AND contestants_right = 1 AND contestants_total = 1
+      ) AS one_for_one_num,
+      COUNT(*) FILTER (
+        WHERE contestants_right = 1 AND contestants_total = 1
+      ) AS one_for_one_den,
+      COUNT(*) FILTER (
+        WHERE result = 3 AND contestants_right = 1 AND contestants_total = 2
+      ) AS one_for_two_num,
+      COUNT(*) FILTER (
+        WHERE contestants_right = 1 AND contestants_total = 2
+      ) AS one_for_two_den,
+      COUNT(*) FILTER (
+        WHERE result = 3 AND contestants_right = 1 AND contestants_total = 3
+      ) AS one_for_three_num,
+      COUNT(*) FILTER (
+        WHERE contestants_right = 1 AND contestants_total = 3
+      ) AS one_for_three_den,
+      COUNT(*) FILTER (
+        WHERE result = 3 AND contestants_right = 2 AND contestants_total = 2
+      ) AS two_for_two_num,
+      COUNT(*) FILTER (
+        WHERE contestants_right = 2 AND contestants_total = 2
+      ) AS two_for_two_den,
+      COUNT(*) FILTER (
+        WHERE result = 3 AND contestants_right = 2 AND contestants_total = 3
+      ) AS two_for_three_num,
+      COUNT(*) FILTER (
+        WHERE contestants_right = 2 AND contestants_total = 3
+      ) AS two_for_three_den,
+      COUNT(*) FILTER (
+        WHERE result = 3 AND contestants_right = 3
+      ) AS three_for_three_num,
+      COUNT(*) FILTER (WHERE contestants_right = 3) AS three_for_three_den,
+      COUNT(*) FILTER (
+        WHERE result = 3 AND first_right
+      ) AS when_first_right_num,
+      COUNT(*) FILTER (WHERE first_right) AS when_first_right_den,
+      COUNT(*) FILTER (
+        WHERE result = 3 AND second_right
+      ) AS when_second_right_num,
+      COUNT(*) FILTER (WHERE second_right) AS when_second_right_den,
+      COUNT(*) FILTER (
+        WHERE result = 3 AND third_right
+      ) AS when_third_right_num,
+      COUNT(*) FILTER (WHERE third_right) AS when_third_right_den,
+      COUNT(*) FILTER (
+        WHERE result = 3 AND first_right IS FALSE
+      ) AS when_first_wrong_num,
+      COUNT(*) FILTER (WHERE first_right IS FALSE) AS when_first_wrong_den,
+      COUNT(*) FILTER (
+        WHERE result = 3 AND second_right IS FALSE
+      ) AS when_second_wrong_num,
+      COUNT(*) FILTER (WHERE second_right IS FALSE) AS when_second_wrong_den,
+      COUNT(*) FILTER (
+        WHERE result = 3 AND third_right IS FALSE
+      ) AS when_third_wrong_num,
+      COUNT(*) FILTER (WHERE third_right IS FALSE) AS when_third_wrong_den,
+      COUNT(*) FILTER (
+        WHERE (
+          (result = 3 OR (second_right IS FALSE AND third_right IS FALSE))
+          AND second_right IS NOT NULL AND third_right IS NOT NULL
+        )
+      ) AS crush_3p_win_num,
+      COUNT(*) FILTER (
+        WHERE second_right IS NOT NULL AND third_right IS NOT NULL
+      ) AS crush_3p_win_den,
+      COUNT(*) FILTER (
+        WHERE (result = 3 OR second_right IS FALSE) AND second_right IS NOT NULL
+      ) AS crush_2p_win_num,
+      COUNT(*) FILTER (
+        WHERE second_right IS NOT NULL
+      ) AS crush_2p_win_den,
+      COUNT(*) FILTER (
+        WHERE second_right IS FALSE AND third_right IS FALSE
+      ) AS second_third_wrong_num,
+      COUNT(*) FILTER (
+        WHERE second_right IS NOT NULL AND third_right IS NOT NULL
+      ) AS second_third_wrong_den,
+      COUNT(*) FILTER (
+        WHERE result = 3 AND first_right IS FALSE
+      ) AS player_right_first_wrong_num,
+      COUNT(*) FILTER (
+        WHERE first_right IS NOT NULL
+      ) AS player_right_first_wrong_den,
+      COUNT(*) FILTER (
+        WHERE first_right IS FALSE AND third_right IS FALSE
+      ) AS first_third_wrong_num,
+      COUNT(*) FILTER (
+        WHERE first_right IS NOT NULL AND third_right IS NOT NULL
+      ) AS first_third_wrong_den,
+      COUNT(*) FILTER (
+        WHERE result = 3 AND first_right IS FALSE and second_right IS FALSE
+      ) AS player_right_first_second_wrong_num,
+      COUNT(*) FILTER (
+        WHERE first_right IS NOT NULL AND second_right IS NOT NULL
+      ) AS player_right_first_second_wrong_den,
+      COUNT(*) FILTER (
+        WHERE first_right IS FALSE and second_right IS FALSE
+      ) AS first_second_wrong_num,
+      COUNT(*) FILTER (
+        WHERE first_right IS NOT NULL AND second_right IS NOT NULL
+      ) AS first_second_wrong_den
+    FROM (
+      SELECT
+        f.result,
+        f.first_right,
+        f.second_right,
+        f.third_right,
+        (
+          CASE WHEN f.first_right THEN 1 ELSE 0 END +
+          CASE WHEN f.second_right THEN 1 ELSE 0 END +
+          CASE WHEN f.third_right THEN 1 ELSE 0 END
+        ) AS contestants_right,
+        (
+          CASE WHEN f.first_right IS FALSE THEN 1 ELSE 0 END +
+          CASE WHEN f.second_right IS FALSE THEN 1 ELSE 0 END +
+          CASE WHEN f.third_right IS FALSE THEN 1 ELSE 0 END
+        ) AS contestants_wrong,
+        (
+          CASE WHEN f.first_right IS NOT NULL THEN 1 ELSE 0 END +
+          CASE WHEN f.second_right IS NOT NULL THEN 1 ELSE 0 END +
+          CASE WHEN f.third_right IS NOT NULL THEN 1 ELSE 0 END
+        ) AS contestants_total
+      FROM finals f
+      INNER JOIN games g ON g.id = f.game_id
+      WHERE g.user_id = #{user.id} AND g.play_type IN (#{play_types_list})
+        AND f.result IN (1, 3)
+    ) q
     "
   end
+  # rubocop:enable MethodLength
 end
+# rubocop:enable ModuleLength
--- app/stats/final_stats_report.rb
+++ app/stats/final_stats_report.rb
@@ -1,115 +1,166 @@
 class FinalStatsReport
-  include FinalQueries, FinalAdviceNumbers, SharedStatsMethods
+  # include FinalQueries, FinalAdviceNumbers, SharedStatsMethods
+  include FinalQueries, SharedStatsMethods
 
   attr_reader :stats
 
   def initialize(user, play_types)
-    finals = ActiveRecord::Base.connection
-                               .select_all(final_query(user, play_types))
-                               .to_hash
+    raw_data = ActiveRecord::Base.connection
+                                 .select_all(final_query(user, play_types))
+                                 .to_hash[0]
 
-    prepare_hash(finals)
-    crunch_numbers(finals)
+    # prepare_hash(finals)
+    # crunch_numbers(finals)
+    @stats = format_data(raw_data)
   end
 
   private
 
-  def prepare_hash(finals)
-    finals.map! do |final|
-      { user_right: final['result'].to_i == 3,
-        contestants_right: [final['first_right'],
-                            final['second_right'],
-                            final['third_right']] }
+  # rubocop:disable MethodLength, AbcSize
+  def format_data(d)
+    { user: make_stat_array(d, 'user'),
+      contestants: [make_stat_array(d, 'contestants'),
+                    make_stat_array(d, 'first'),
+                    make_stat_array(d, 'second'),
+                    make_stat_array(d, 'third')],
+      by_get_rate: [[make_stat_array(d, 'zero_for_zero'),
+                     make_stat_array(d, 'zero_for_one'),
+                     make_stat_array(d, 'zero_for_two'),
+                     make_stat_array(d, 'zero_for_three')],
+                    [nil,
+                     make_stat_array(d, 'one_for_one'),
+                     make_stat_array(d, 'one_for_two'),
+                     make_stat_array(d, 'one_for_three')],
+                    [nil, nil,
+                     make_stat_array(d, 'two_for_two'),
+                     make_stat_array(d, 'two_for_three')],
+                    [nil, nil, nil,
+                     make_stat_array(d, 'three_for_three')]],
+      when_right: [nil,
+                   make_stat_array(d, 'when_first_right'),
+                   make_stat_array(d, 'when_second_right'),
+                   make_stat_array(d, 'when_third_right')],
+      when_wrong: [nil,
+                   make_stat_array(d, 'when_first_wrong'),
+                   make_stat_array(d, 'when_second_wrong'),
+                   make_stat_array(d, 'when_third_wrong')],
+      advice: make_advice_hash(d) }
+  end
+  # rubocop:enable MethodLength, AbcSize
+
+  def make_stat_array(data, attr)
+    num = data[attr + '_num'] || 0
+    den = data[attr + '_den'] || 0
+    [num, den, quotient(num, den)]
+  end
+
+  def make_advice_hash(d)
+    advice = {}
+    %w(
+      crush_3p_win crush_2p_win second_third_wrong player_right_first_wrong
+      first_third_wrong player_right_first_second_wrong first_second_wrong
+    ).each do |situation|
+      advice[situation.to_sym] = make_stat_array(d, situation)
     end
+    advice
   end
 
-  def crunch_numbers(finals)
-    @stats = { user: [0, 0], contestants: [nil, [0, 0], [0, 0], [0, 0]],
-               by_get_rate: [[[0, 0], [0, 0], [0, 0], [0, 0]],
-                             [nil,    [0, 0], [0, 0], [0, 0]],
-                             [nil,    nil,    [0, 0], [0, 0]],
-                             [nil,    nil,    nil,    [0, 0]]],
-               when_right: [nil, [0, 0], [0, 0], [0, 0]],
-               when_wrong: [nil, [0, 0], [0, 0], [0, 0]] }
+  # def prepare_hash(finals)
+  #   finals.map! do |final|
+  #     { user_right: final['result'].to_i == 3,
+  #       contestants_right: [final['first_right'],
+  #                           final['second_right'],
+  #                           final['third_right']] }
+  #   end
+  # end
 
-    initialize_advice_numbers
+  # def crunch_numbers(finals)
+  #   @stats = { user: [0, 0], contestants: [nil, [0, 0], [0, 0], [0, 0]],
+  #              by_get_rate: [[[0, 0], [0, 0], [0, 0], [0, 0]],
+  #                            [nil,    [0, 0], [0, 0], [0, 0]],
+  #                            [nil,    nil,    [0, 0], [0, 0]],
+  #                            [nil,    nil,    nil,    [0, 0]]],
+  #              when_right: [nil, [0, 0], [0, 0], [0, 0]],
+  #              when_wrong: [nil, [0, 0], [0, 0], [0, 0]] }
 
-    process_finals(finals)
-    post_processing
-  end
+  #   initialize_advice_numbers
 
-  def process_finals(finals)
-    finals.each do |final|
-      add_raw_results(final)
-      add_by_get_rate(final)
-      add_by_contestant_result(final)
-      add_advice_numbers(final)
-    end
-  end
+  #   process_finals(finals)
+  #   post_processing
+  # end
 
-  def add_raw_results(final)
-    add_final_to(@stats[:user], final)
-    (1..3).each { |i| add_final_to(@stats[:contestants][i], final, i) }
-  end
+  # def process_finals(finals)
+  #   finals.each do |final|
+  #     add_raw_results(final)
+  #     add_by_get_rate(final)
+  #     add_by_contestant_result(final)
+  #     add_advice_numbers(final)
+  #   end
+  # end
 
-  def add_by_get_rate(final)
-    correct_responders = 0
-    participants = 0
+  # def add_raw_results(final)
+  #   add_final_to(@stats[:user], final)
+  #   (1..3).each { |i| add_final_to(@stats[:contestants][i], final, i) }
+  # end
 
-    final[:contestants_right].each do |result|
-      correct_responders += 1 if result
-      participants += 1 unless result.nil?
-    end
+  # def add_by_get_rate(final)
+  #   correct_responders = 0
+  #   participants = 0
 
-    add_final_to(@stats[:by_get_rate][correct_responders][participants], final)
-  end
+  #   final[:contestants_right].each do |result|
+  #     correct_responders += 1 if result
+  #     participants += 1 unless result.nil?
+  #   end
 
-  def add_by_contestant_result(final)
-    (1..3).each do |i|
-      case final[:contestants_right][i - 1]
-      when true then add_final_to(@stats[:when_right][i], final)
-      when false then add_final_to(@stats[:when_wrong][i], final)
-      end
-    end
-  end
+  #   add_final_to(@stats[:by_get_rate][correct_responders][participants], final)
+  # end
 
-  def add_final_to(arr, final, player = nil)
-    result = player ? final[:contestants_right][player - 1] : final[:user_right]
+  # def add_by_contestant_result(final)
+  #   (1..3).each do |i|
+  #     case final[:contestants_right][i - 1]
+  #     when true then add_final_to(@stats[:when_right][i], final)
+  #     when false then add_final_to(@stats[:when_wrong][i], final)
+  #     end
+  #   end
+  # end
 
-    case result
-    when true then arr[0] += 1
-    when false then arr[1] += 1
-    end
-  end
+  # def add_final_to(arr, final, player = nil)
+  #   result = player ? final[:contestants_right][player - 1] : final[:user_right]
 
-  def post_processing
-    # Add the contestants' overall stats.
-    sc = @stats[:contestants]
-    sc[0] = sc[1..3].transpose.map { |arr| arr.reduce(:+) }
+  #   case result
+  #   when true then arr[0] += 1
+  #   when false then arr[1] += 1
+  #   end
+  # end
 
-    add_averages
+  # def post_processing
+  #   # Add the contestants' overall stats.
+  #   sc = @stats[:contestants]
+  #   sc[0] = sc[1..3].transpose.map { |arr| arr.reduce(:+) }
 
-    @stats[:advice] = @advice
-  end
+  #   add_averages
 
-  def add_averages
-    # Assemble the arrays that contain get rates.
-    arrs = assemble_arrays
+  #   @stats[:advice] = @advice
+  # end
 
-    # Change the format of each array from [right, wrong] to [right, total]
-    # and tack on the get rate as a float between 0 and 1.
-    arrs.each do |arr|
-      arr[1] += arr[0]
-      arr << quotient(arr[0], arr[1])
-    end
-  end
+  # def add_averages
+  #   # Assemble the arrays that contain get rates.
+  #   arrs = assemble_arrays
 
-  def assemble_arrays
-    ([@stats[:user]] +
-     @stats[:contestants] +
-     @stats[:by_get_rate].flatten(1) +
-     @stats[:when_right] +
-     @stats[:when_wrong] +
-     @advice.values).compact
-  end
+  #   # Change the format of each array from [right, wrong] to [right, total]
+  #   # and tack on the get rate as a float between 0 and 1.
+  #   arrs.each do |arr|
+  #     arr[1] += arr[0]
+  #     arr << quotient(arr[0], arr[1])
+  #   end
+  # end
+
+  # def assemble_arrays
+  #   ([@stats[:user]] +
+  #    @stats[:contestants] +
+  #    @stats[:by_get_rate].flatten(1) +
+  #    @stats[:when_right] +
+  #    @stats[:when_wrong] +
+  #    @advice.values).compact
+  # end
 end
