320159960aad9a5f130160ff30cc8f036f487783
steve-mcclellan <steve470@gmail.com>
2019-09-08 13:27:27 -0400

    Add failing tests for topic-stats-page feature

--- test/controllers/stats_controller_test.rb
+++ test/controllers/stats_controller_test.rb
@@ -58,4 +58,97 @@ class StatsControllerTest < ActionController::TestCase
                            show_date_from: 'not-a-date' }
     assert_response :success
   end
+
+  test 'should redirect topic when not logged in' do
+    get :topic, params: { name: 'InQuotes' }
+    assert_not flash.empty?
+    assert_redirected_to login_url
+  end
+
+  test 'should display valid topic when logged in' do
+    log_in_here(@user)
+    get :topic, params: { name: 'InQuotes' }
+    assert flash.empty?
+    assert_response :success
+    assert_select 'title', 'J! Scorer - Topic stats'
+    assert_includes response.body, 'Potpourri'
+    assert_includes response.body, @user.shared_stats_name # should be email
+  end
+
+  test 'should 404 topic when given a non-existent topic' do
+    log_in_here(@user)
+    get :topic, params: { name: 'IAmNotATopic' }
+    assert_response :not_found
+  end
+
+  test 'should get valid sample topic when not logged in' do
+    get :sample_topic, params: { name: 'Lowbrow' }
+    assert flash.empty?
+    assert_response :success
+    assert_includes response.body, 'Lowbrow'
+    assert_includes response.body, @user.shared_stats_name # should be email
+  end
+
+  test 'should get valid sample topic when logged in' do
+    log_in_here(@other_user)
+    get :sample_topic, params: { name: 'Lowbrow' }
+    assert flash.empty?
+    assert_response :success
+    assert_includes response.body, 'Lowbrow'
+    assert_includes response.body, @user.shared_stats_name # should be email
+  end
+
+  test 'should 404 non-existent sample topic' do
+    get :sample_topic, params: { name: 'TrinidadianProfessionalIchthyologists' }
+    assert_response :not_found
+    log_in_here(@other_user)
+    get :sample_topic, params: { name: 'TrinidadianProfessionalIchthyologists' }
+    assert_response :not_found
+  end
+
+  test 'shared topic should 404 bad user name' do
+    get :shared_topic, params: { name: 'NotAUser', topic: 'General' }
+    assert_response :not_found
+    log_in_here(@user)
+    get :shared_topic, params: { name: 'NotAUser', topic: 'General' }
+    assert_response :not_found
+  end
+
+  test 'shared topic should 404 for summary-stats-only user' do
+    get :shared_topic, params: { name: @other_user.shared_stats_name,
+                                 topic: 'General' }
+    assert_response :not_found
+    log_in_here(@user)
+    get :shared_topic, params: { name: @other_user.shared_stats_name,
+                                 topic: 'General' }
+    assert_response :not_found
+  end
+
+  test 'shared topic should 404 on non-existent topic' do
+    @other_user.share_detailed_stats = true
+    get :shared_topic, params: { name: @other_user.shared_stats_name,
+                                 topic: 'NotATopic' }
+    assert_response :not_found
+    log_in_here(@user)
+    get :shared_topic, params: { name: @other_user.shared_stats_name,
+                                 topic: 'NotATopic' }
+    assert_response :not_found
+  end
+
+  test 'shared topic should actually work if request is good' do
+    @other_user.share_detailed_stats = true
+    get :shared_topic, params: { name: 'Highbrow' }
+    assert flash.empty?
+    assert_response :success
+    assert_select 'title', 'J! Scorer - Shared topic stats'
+    assert_includes response.body, 'Potpourri'
+    assert_includes response.body, @user.email # should be shared_stats_name
+    log_in_here(@user)
+    get :shared_topic, params: { name: 'Highbrow' }
+    assert flash.empty?
+    assert_response :success
+    assert_select 'title', 'J! Scorer - Shared topic stats'
+    assert_includes response.body, 'Potpourri'
+    assert_includes response.body, @user.email # should be shared_stats_name
+  end
 end
