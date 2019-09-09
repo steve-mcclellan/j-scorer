dd06fd52f0404a0ef425069ccd404bc7e60f2d83
steve-mcclellan <steve470@gmail.com>
2019-09-08 22:53:32 -0400

    Make category title appear for Finals, as well

--- app/stats/topic_details.rb
+++ app/stats/topic_details.rb
@@ -38,7 +38,7 @@ class TopicDetails
         COALESCE(gOne.game_id, gTwo.game_id) AS game_id,
         COALESCE(gOne.date_played, gTwo.date_played) AS date_played,
         s.board_position,
-        s.title,
+        COALESCE(s.title, f.category_title) AS title,
         s.result1,
         s.result2,
         s.result3,
