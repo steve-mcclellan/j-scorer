5f7db85eb4fa323329255107fa449a05ec1e8bc4
steve-mcclellan <steve470@gmail.com>
2020-04-19 16:35:43 -0400

    Add logged-in-user info to request logs

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -16,13 +16,9 @@ module SessionsHelper
   # Returns the user via the current session or a remember token cookie.
   def current_user
     if (user_id = session[:user_id])
-      @current_user ||= User.find_by(id: user_id)
+      get_current_user_from_session(user_id)
     elsif (user_id = cookies.signed[:user_id])
-      user = User.find_by(id: user_id)
-      if user&.authenticated?(:remember, cookies[:remember_token])
-        log_in user
-        @current_user = user
-      end
+      get_current_user_from_cookie(user_id)
     end
   end
 
@@ -59,5 +55,26 @@ module SessionsHelper
   # Stores the URL that the user was trying to access.
   def store_location
     session[:forwarding_url] = request.url if request.get?
+  end
+
+  private
+
+  def get_current_user_from_session(user_id)
+    return @current_user if @current_user
+
+    @current_user = User.find_by(id: user_id)
+    logger.info do
+      "User #{user_id} (#{@current_user.email}) inferred from session."
+    end
+    @current_user
+  end
+
+  def get_current_user_from_cookie(user_id)
+    user = User.find_by(id: user_id)
+    return unless user&.authenticated?(:remember, cookies[:remember_token])
+
+    log_in user
+    logger.info { "Logged in user #{user_id} (#{user.email}) via cookie." }
+    @current_user = user
   end
 end
