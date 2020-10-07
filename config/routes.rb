37120073988ea6d6ac70d1c34dc9d10b1d5b31f5
steve-mcclellan <steve470@gmail.com>
2020-10-06 20:41:10 -0400

    Don't show page controls if there's only one page

--- config/routes.rb
+++ config/routes.rb
@@ -16,11 +16,6 @@ Rails.application.routes.draw do
   get 'sample/topic/:topic' => 'stats#sample_topic', as: :sample_topic
   get 'shared/:user/topic/:topic' => 'stats#shared_topic', as: :shared_topic
 
-  # TODO: Move these into the XHR section
-  get 'stats/games' => 'stats#games', as: :games
-  get 'sample/games' => 'stats#sample_games', as: :sample_games
-  get 'shared/:user/games' => 'stats#shared_games', as: :shared_games
-
   get 'login' => 'sessions#new'
   post 'login' => 'sessions#create'
   delete 'logout' => 'sessions#destroy'
@@ -45,5 +40,9 @@ Rails.application.routes.draw do
 
     patch 'filters' => 'users#update_user_filters'
     patch 'sharing' => 'users#update_sharing_status'
+
+    get 'stats/games' => 'stats#games', as: :games
+    get 'sample/games' => 'stats#sample_games', as: :sample_games
+    get 'shared/:user/games' => 'stats#shared_games', as: :shared_games
   end
 end
