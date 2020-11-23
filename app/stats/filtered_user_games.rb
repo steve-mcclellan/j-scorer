f6cf36b0c458ecf67dd131a75d947912f0d47a5c
steve-mcclellan <steve470@gmail.com>
2020-11-22 19:30:07 -0500

    Add column names and generators

--- app/stats/filtered_user_games.rb
+++ app/stats/filtered_user_games.rb
@@ -1,23 +1,33 @@
 class FilteredUserGames
   attr_reader :games
 
-  # TODO: Flesh this out into actual code.
-  # TODO: Use the temp score-ordering code as an example.
-  # pa: date_played
-  # rr: rerun
-  # sd: show_date
-  # pt: play_type
-  # TODO: cs: round_one_score + 2 * round_two_score
-  # ro: round_one_score
-  # rt: round_two_score
-  # TODO: dr: CASE WHEN dd1_result = 7 THEN 1 ELSE 0 END +
-  #                     dd2a_result
-  #                     dd2b_result
-  # TODO: dw: CASE WHEN dd1_result IN (5, 6) THEN 1 ELSE 0 END +
-  #                     dd2a_result
-  #                     dd2b_result
-  # fi: final_result
-  #
+  COLUMN_NAMES = {
+    'pa' => 'date_played',
+    'rr' => 'rerun',
+    'sd' => 'show_date',
+    'pt' => 'play_type',
+    'cs' => 'coryat',
+    'ro' => 'round_one_score',
+    'rt' => 'round_two_score',
+    'dr' => 'dd_right',
+    'dw' => 'dd_wrong',
+    'fi' => 'final_result'
+  }.freeze
+
+  def dd_counter(condition, column_name)
+    "
+    CASE WHEN dd1_result #{condition} THEN 1 ELSE 0 END +
+    CASE WHEN dd2a_result #{condition} THEN 1 ELSE 0 END +
+    CASE WHEN dd2b_result #{condition} THEN 1 ELSE 0 END AS #{column_name}
+    "
+  end
+
+  NONCE_COLUMNS = {
+    'cs' => 'round_one_score + 2 * round_two_score AS coryat',
+    'dr' => dd_counter('= 7', 'dd_right'),
+    'dw' => dd_counter('IN (5, 6)', 'dd_wrong')
+  }.freeze
+
   def initialize(user, params, filters, play_types)
     @filters = filters
     @play_types = play_types
