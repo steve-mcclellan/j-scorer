320ab2aab4f071c12100d19a7d330496a3518643
steve-mcclellan <steve470@gmail.com>
2020-12-08 20:53:52 -0500

    Switch to better values for the real world

--- app/controllers/backups_controller.rb
+++ app/controllers/backups_controller.rb
@@ -11,7 +11,7 @@ class BackupsController < ApplicationController
 
   def status
     job_status = ActiveJob::Status.get(params[:backup_id])
-    logger.warn { "About to return backup job status: #{job_status}" }
+    logger.info { "About to return backup job status: #{job_status}" }
     render json: job_status
   end
 
@@ -38,7 +38,7 @@ class BackupsController < ApplicationController
 
   def restore_progress
     job_status = ActiveJob::Status.get(params[:restore_id])
-    logger.warn { "About to return restore job status: #{job_status}" }
+    logger.info { "About to return restore job status: #{job_status}" }
     render json: job_status
   end
 end
